0e9a085a705b1a9175a65a173b8c95402cbdb698
Aluno SENAI - AREIAS <aluno.areias@SISTEMAFIEPE.LOCAL>
2024-09-26 21:43:17 -0300

    Aula 26/05 finalizado

---
+++ js_funcoes.js
@@ -0,0 +1,10 @@
+function abrirMenu(){
+    var minhaDiv= document.getElementById("menu_lateral");
+    minhaDiv.classList.add("aparecer");
+    }
+
+
+    function fecharMenu(){
+        var minhaDiv = document.getElementById("menu_lateral");
+        minhaDiv.classList.remove("aparecer");
+    }
